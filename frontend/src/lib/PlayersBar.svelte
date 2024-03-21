8aac2f8466139e657fa5ebbc5bec23ee7468bdb0
Louis Marceron <louis.marceron@protonmail.com>
2024-03-21 13:42:49 +0100

    refactor: improve styling

---
+++ frontend/src/lib/PlayersBar.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let player1Name = 'Player 1';
+	export let player2Name = 'Player 2';
+	export let player1Score = 0;
+	export let player2Score = 0;
+</script>
+
+<div id="players-bar">
+	<p class="player">{player1Name} - {player1Score}</p>
+	<p class="player">{player2Score} - {player2Name}</p>
+</div>
+
+<style>
+	#players-bar {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+		align-self: self-end;
+	}
+
+	.player {
+		font-size: 2.5rem;
+		text-transform: uppercase;
+	}
+</style>
