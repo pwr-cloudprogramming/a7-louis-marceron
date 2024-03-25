ca6e670b75f30132970d8156e2c1ba2a171d5a87
Louis Marceron <louis.marceron@protonmail.com>
2024-03-25 23:20:14 +0100

    fix: prevent home to create a ws connection

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,29 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
-	let socket: WebSocket;
-
-	onMount(() => {
-		socket = new WebSocket('ws://localhost:8080/online');
-
-		socket.onopen = () => {
-			console.log('Socket opened!');
-			socket.send('Hello server!');
-			socket.send(JSON.stringify({ message: 'after it is open' }));
-		};
-
-		socket.onerror = () => {
-			console.log("Couldn't connect to the socket");
-		};
-
-		console.log('Before it is open');
-		// socket.send(JSON.stringify({ message: 'before it is open' }));
-	});
-
-	const handleOnPress = () => {
-		console.log('sending messagr....');
-		socket.send(JSON.stringify({ message: 'haahhahah' }));
-	};
 </script>
 
 <svelte:head>
@@ -38,7 +13,6 @@
 		<a href="/multiplayer">Online</a>
 	</nav>
 </section>
-<button on:click={handleOnPress}>WS request</button>
 
 <style>
 	section {
